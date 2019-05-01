c3e2307e4614c45f28a43be8e632929ec0fed595
mpj <maple.develops@gmail.com>
2019-05-01 10:13:10 -0400

    Create questions from UI

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,6 @@
 class Question < ActiveRecord::Base
-    validates :question, presence :true, length: { minimum: 10, maximum: 300 }
-    validates :name, presence :true, length: { minimum: 3, maximum: 45 }
-    
+    validates :question, presence: :true, length: { minimum: 10, maximum: 300 }
+    validates :name, presence: :true, length: { minimum: 3, maximum: 45 }
     # To do: Add validation for website
     # with gem https://github.com/perfectline/validates_url
 end 
--- db/migrate/20190423004754_add_anonymous_check.rb
+++ db/migrate/20190423004754_add_anonymous_check.rb
@@ -1,5 +1,5 @@
 class AddAnonymousCheck < ActiveRecord::Migration[5.2]
   def change
-    add_column :question, :stay_anonymous, :boolean
+    add_column :questions, :stay_anonymous, :boolean
   end
 end
