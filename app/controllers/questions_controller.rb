a463e72ad1fcd7ebc66eb2137c685af649c61589
mpj <maple.develops@gmail.com>
2019-05-01 11:32:15 -0400

    Add creation, edit and update methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,14 +3,39 @@ class QuestionsController < ApplicationController
         @question = Question.new
     end
     
-    def create
+    def create           
         @question = Question.new(question_params)
-        @question.save
+        if @question.save
+            flash[:notice] = "Question was successfully created"
+            redirect_to question_path(@question)
+        else
+            byebug
+            render 'new'
+        end
+    end  
+
+    def show
+        @question = Question.find(params[:id])
+    end 
+
+    def edit
+        @question = Question.find(params[:id])
     end     
+    
+    def update
+        @question = Question.find(params[:id])
+        if @question.update(question_params)
+            flash[:notice] = "Your question was successfully updated 🙌"
+            redirect_to question_path(@question)
+        else
+            render 'edit'
+        end
+    end        
+
     private
 
     def question_params
-        params.require(:question).permit(:question, :name)
+        params.require(:question).permit(:question, :name, :stay_anonymous)
     end 
 
 end 
