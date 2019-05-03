3d359ccccedcde54574edda67edc9b3ce11725fa
mpj <maple.develops@gmail.com>
2019-05-03 15:49:04 -0400

    Add meow (error handler)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
-
+require 'logger'
 class ApplicationController < ActionController::Base
+  def initialize
+    logger = Logger.new('MyLog.log')
+  end
+
+  # Meow logs error with emojis cause why not
+  def meow(error)
+    if error
+      logger.error("🚧 Error: " + error + " 🚧")
+    else 
+      logger.error("🚧 Error occurred!! 🚧")
+  end
 end
