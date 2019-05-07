f56fc8e8c7fea9ede0bce8c3aad08900a6649c2d
mpj <maple.develops@gmail.com>
2019-05-07 13:07:51 -0400

    Fix bug where bootstrap was not showing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,11 @@
 
 require 'logger'
 class ApplicationController < ActionController::Base
-  def initialize
-    logger = Logger.new('QuestiongLog.log')
-  end
 
   # Meow logs error with emojis cause why not
   def meow(error)
+    logger = Logger.new('QuestiongLog.log')
+
     if error
       logger.error('🚧 Error: ' + error + ' 🚧')
     else
