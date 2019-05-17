5aa74020519a9ae70929286be1339909d63e1cab
mpj <maple.develops@gmail.com>
2019-05-17 16:04:19 -0400

    Create login UI view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'about', to: 'about#about'
 
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users
   resources :questions
   resources :answers
