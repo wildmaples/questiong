52e859d6e170cee5657f274ce85acc74ff993266
mpj <maple.develops@gmail.com>
2019-05-21 11:43:50 -0400

    Add if-block for logged in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            flash[:success] = "You have successfully logged in"
-            redirect_to user_path(user)
+            flash[:success] = "You have successfully logged in 👋🏼"
+            redirect_to root_path
         else
             flash.now[:danger] = "There was something wrong with your login information"
             render 'new'
