73c41565c014019679e87e3894707709e9b8a906
Maple Ong <maple.ong@shopify.com>
2020-05-03 17:58:58 -0400

    Add user controller test

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @username = "awildmaples"
+  end
+  test 'should not save user without proper params' do
+    user = User.new
+    assert_not user.save
+  end
+
+  test 'should get #new' do
+    get '/users/new'
+    assert_response :ok
+  end
+
+  test 'should create user with proper params' do
+    assert_difference('User.count') do
+      post users_url, params: { user:
+        {
+          username: @username,
+          email: 'maple@questiong.com',
+          password: 'password_should_be_minimum_9_characters',
+        }
+      }
+    end
+
+    assert_redirected_to questions_path
+    assert_match @username, flash[:notice]
+  end
+
+  test 'should not create user without proper params' do
+    assert_no_difference('User.count') do
+      post users_url, params: { user:
+        {
+          username: @username,
+          email: 'maple@questiong.com',
+          password: 'password',
+        }
+      }
+    end
+  end
+end
